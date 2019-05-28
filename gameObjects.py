67ea94251779f2a8f93926c55f96a456660e1ebc
Kevin Ha <38023023+kha4@users.noreply.github.com>
2019-05-28 16:11:41 -0700

    Add files via upload

--- gameObjects.py
+++ gameObjects.py
@@ -15,29 +15,56 @@ def createObjectList(newGame):
 	toilet = {'Name': 'Toilet', 'Description': 'The stench is overpowering. It has not been cleaned in months.', 'Location': 'Prison Cell', 'Movable': 'n'}
 	newGame.addObject(toilet)
 
-	sink = {'Name': 'Sink', 'Description': 'It is a putrid looking sink. At least there is clean water.', 'Location': 'Prison Cell', 'Movable': 'n'}
+	sink = {'Name': 'Sink', 'Description': 'It is a putrid looking sink. At least the water is somewhat clear.', 'Location': 'Prison Cell', 'Movable': 'n'}
 	newGame.addObject(sink)
 
-	airVent = {'Name': 'Air Vent', 'Description': 'You found a key.', 'ShortDesc': 'Seems like there is room for a small person to fit in the air vent.', 'Location': 'Prison Cell', 'Movable': 'n', 'HiddenItem': 'Key'}
+	airVent = {'Name': 'Air Vent', 'Description': 'Seems like there is room for a small person to fit in the air vent.', 'Location': 'Prison Cell', 'Movable': 'n'}
 	newGame.addObject(airVent)
 
-	key = {'Name': 'Key', 'Description': 'Someone seems to have dropped this key. I wonder what it opens.', 'Location': 'Prison Cell', 'Movable': 'y'}
+	drainPipes = {'Name': 'Drain Pipes', 'Description': 'You found a key.', 'ShortDesc': 'It seems moist and moldy. Nothing interesting here.', 'Location': 'Cell House Roof', 'Movable': 'n', 'HiddenItem': 'Key'}
+	newGame.addObject(drainPipes)
+
+	skyLights = {'Name': 'Sky Lights', 'Description': 'The light illuminating from the sky looks beautiful despite the gloomy looking clouds.', 'Location': 'Cell House Roof', 'Movable': 'n'}
+	newGame.addObject(skyLights)
+
+	key = {'Name': 'Key', 'Description': 'Someone seems to have left this key. I wonder what it opens.', 'Location': 'Cell House Roof', 'Movable': 'y'}
 	newGame.addObject(key)
 
-	lockingMechanism = {'Name': 'Locking Mechanism', 'Description': 'Seems like the locking mechanism has already been deactivated. Doors shoould be open in Cell Block A.', 'Location': 'Cell Block A', 'Movable': 'n'}
+	lockingMechanism = {'Name': 'Locking Mechanism', 'Description': 'Seems like this can open locked doors.', 'ShortDesc': 'Seems like the locking mechanism has already been deactivated. Doors are open in Cell Block A.', 'Location': 'Cell Block A', 'Movable': 'n'}
 	newGame.addObject(lockingMechanism)
+
+	guard = {'Name': 'Guard', 'Description': 'The guard is onto you!', 'Location': 'Cell Block A', 'Movable': 'n'}
+	newGame.addObject(guard)
+
+	tables = {'Name': 'Tables', 'Description': 'There is gum and signs of leftover food stained on these tables.', 'Location': 'Mess Hall', 'Movable': 'n'}
+	newGame.addObject(tables)
+
+	dishSink = {'Name': 'Dish Sink', 'Description': 'Someone needs to do the dishes.', 'Location': 'Kitchen', 'Movable': 'n'}
+	newGame.addObject(dishSink)
+
+	pantry = {'Name': 'Pantry', 'Description': 'Looks like the rats have made a home in here. Smells disgusting.', 'Location': 'Kitchen', 'Movable': 'n'}
+	newGame.addObject(pantry)
 
 	bookShelves = {'Name': 'Book Shelves', 'Description': 'The book shelves are filled with old, dusty books. Nothing looks interesting here.', 'Location': 'Library', 'Movable': 'n'}
 	newGame.addObject(bookShelves)
 
-	desk = {'Name': 'Desk', 'Description': 'You found glue. Looks like an ordinary desk. There are books scattered around. Someone was reading \"Rafts for Dummies\". Looks intriguing.', 'Location': 'Library', 'Movable': 'n', 'HiddenItem': 'Glue'}
+	desk = {'Name': 'Desk', 'Description': 'You found glue.', 'Shortdesc': ' Looks like an ordinary desk. There are books scattered around. Someone was reading \"Rafts for Dummies\". Looks intriguing.', 'Location': 'Library', 'Movable': 'n', 'HiddenItem': 'Glue'}
 	newGame.addObject(desk)
 
 	bookCart = {'Name': 'Book Cart', 'Description': 'The wheel is broken. It has not been used in months.', 'Location': 'Library', 'Movable': 'n'}
 	newGame.addObject(bookCart)
 
-	glue = {'Name': 'Glue', 'Description': 'Seems like very durable glue. You probably should not glue your fingers together.', 'Location': 'Library', 'Movable': 'y'}
+	glue = {'Name': 'Glue', 'Description': 'Seems like very durable glue. You should probably not glue your fingers together.', 'Location': 'Library', 'Movable': 'y'}
 	newGame.addObject(glue)
+
+	doorLockControlPanel = {'Name': 'Door Lock Control Panel', 'Description': 'Looks like it opens the door to the Armory.', 'Location': 'Sally Port', 'Movable': 'n'}
+	newGame.addObject(doorLockControlPanel)
+
+	metalDetector = {'Name': 'Metal Detector', 'Description': 'Seems to be out of order.', 'Location': 'Sally Port', 'Movable': 'n'}
+	newGame.addObject(metalDetector)
+
+	gunRacks = {'Name': 'Gun Racks', 'Description': 'Looks like there are only large guns here. It would be too conspicuous to sneak around with one. Better to just leave them here.', 'Location': 'Armory', 'Movable': 'n'}
+	newGame.addObject(gunRacks)
 
 	visitationPhone = {'Name': 'Visitation Phone', 'Description': 'You cannot hear anything. There is no one on the other side.', 'Location': 'Visiting Room', 'Movable': 'n'}
 	newGame.addObject(visitationPhone)
@@ -47,6 +74,8 @@ def createObjectList(newGame):
 
 	raincoats = {'Name': 'Raincoats', 'Description': 'These raincoats seem to be extremely reliable. No water would be able to get on you. Looks like they were probably made for visitors', 'Location': 'Visiting Room', 'Movable': 'y'}
 	newGame.addObject(raincoats)
+
+
 
 	fence = {'Name': 'Fence', 'Description': 'This barbed wire fence is keeping prisoners inside of the yard. Does not seem too high though. You can see the shore from here.', 'Location': 'Yard', 'Movable': 'n'}
 	newGame.addObject(fence)
